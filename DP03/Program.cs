58e56eaa1b22a09da4c36649126601c3a932fb4a
Richard Chalk <barmybrit@gmail.com>
2025-06-23 15:58:50 +0200

    Add project files.

---
+++ DP03/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace DP03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Skapa karaktär med svärd
+            var fighter = new Fighter(new MeleeAttack());
+            fighter.PerformAttack();
+
+            // Byt till pilbåge
+            fighter.SetAttackStrategy(new RangedAttack());
+            fighter.PerformAttack();
+
+            // Byt till magi
+            fighter.SetAttackStrategy(new MagicAttack());
+            fighter.PerformAttack();
+        }
+
+        public interface IAttack
+        {
+            void Attack();
+        }
+
+        public class MeleeAttack : IAttack
+        {
+            public void Attack()
+            {
+                Console.WriteLine("Utför ett kraftfullt svärdslag!");
+            }
+        }
+
+        public class RangedAttack : IAttack
+        {
+            public void Attack()
+            {
+                Console.WriteLine("Skjuter en pil från avstånd!");
+            }
+        }
+
+        public class MagicAttack : IAttack
+        {
+            public void Attack()
+            {
+                Console.WriteLine("Kastar en eldboll!");
+            }
+        }
+
+        public class Fighter
+        {
+            private IAttack _attack;
+
+            public Fighter(IAttack attack)
+            {
+                _attack = attack;
+            }
+
+            public void PerformAttack()
+            {
+                _attack.Attack();
+            }
+
+            // Byt attack under spelets gång
+            public void SetAttackStrategy(IAttack newAttack)
+            {
+                _attack = newAttack;
+            }
+        }
+    }
+}
